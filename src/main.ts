e06a8be908cf65a04c0271907314fcfaee7205ec
Owen Greenhalgh <og2701@hotmail.com>
2026-05-10 18:18:34 +0100

    Allow production frontend CORS origins

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+function normalizeOrigin(origin: string) {
+  return origin.trim().replace(/\/+$/, '');
+}
+
+function getCorsOrigins() {
+  return [
+    'http://localhost:5173',
+    'https://verbilo.co.uk',
+    'https://www.verbilo.co.uk',
+    ...(process.env.FRONTEND_URL ?? '').split(','),
+    ...(process.env.FRONTEND_URLS ?? '').split(','),
+  ]
+    .map(normalizeOrigin)
+    .filter(Boolean);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', process.env.FRONTEND_URL].filter(
-      Boolean,
-    ) as string[],
+    origin: getCorsOrigins(),
   });
   await app.listen(process.env.PORT ?? 3000);
 }
